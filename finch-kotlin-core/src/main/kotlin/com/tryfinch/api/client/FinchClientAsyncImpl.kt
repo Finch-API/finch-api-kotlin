639fac536177f907f3061e5202bcd9d4d426f8ae
Tomer Aberbach <tomer@stainless.com>
2025-03-07 10:49:40 -0500

    fix: compilation error

--- finch-kotlin-core/src/main/kotlin/com/tryfinch/api/client/FinchClientAsyncImpl.kt
+++ finch-kotlin-core/src/main/kotlin/com/tryfinch/api/client/FinchClientAsyncImpl.kt
@@ -11,7 +11,7 @@ import com.tryfinch.api.core.handlers.withErrorHandler
 import com.tryfinch.api.core.http.HttpMethod
 import com.tryfinch.api.core.http.HttpRequest
 import com.tryfinch.api.core.http.HttpResponse.Handler
-import com.tryfinch.api.core.json
+import com.tryfinch.api.core.http.json
 import com.tryfinch.api.errors.FinchError
 import com.tryfinch.api.errors.FinchException
 import com.tryfinch.api.services.async.AccessTokenServiceAsync
