99e16a7804b831199e51c5592f2f33b51722b662
Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
2026-08-21 18:04:01 -0500

    release: 9.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 
 allprojects {
     group = "com.tryfinch.api"
-    version = "9.8.0" // x-release-please-version
+    version = "9.8.1" // x-release-please-version
 }
 
 subprojects {
